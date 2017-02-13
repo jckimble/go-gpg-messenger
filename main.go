f47dd9b606b083650dbecf3719c155b8c9f292f9
James C Kimble <jckimble601@gmail.com>
2017-02-12 19:57:59 -0600

    create queries

--- main.go
+++ main.go
@@ -17,35 +17,39 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("location","/ws")
 }
+const (
+	MySQL = iota
+	Postgres
+	SQLite
+)
+var driver = map[int]string{
+	0: "mysql",
+	1: "postgres",
+	2: "sqlite3",
+}
+var CREATEUSERS = map[int]string{
+	MySQL: 'CREATE TABLE IF NOT EXISTS users (id INT UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(255), domain VARCHAR(255), password VARCHAR(255), key VARCHAR(16), port INT UNSIGNED NOT NULL, PRIMARY KEY (id))'
+	Postgres: 'CREATE SEQUENCE users_seq; CREATE TABLE IF NOT EXISTS users (id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('users_seq'), name VARCHAR(255), domain VARCHAR(255), password VARCHAR(255), port INT CHECK (port > 0) NOT NULL, PRIMARY KEY (id)) create index VARCHAR on users(16);',
+	SQLite: 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), domain VARCHAR(255), password VARCHAR(255), key VARCHAR(16), port INTEGER UNSIGNED NOT NULL)',
+}
+var CREATEMESSAGES = map[int]string{
+	MySQL: 'CREATE TABLE IF NOT EXISTS messages (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `to` INT UNSIGNED NOT NULL, `from` INT UNSIGNED NOT NULL, `time` INT UNSIGNED NOT NULL, `type` VARCHAR(255), `message` longtext NULL, PRIMARY KEY (id))',
+	Postgres: "CREATE SEQUENCE messages_seq; CREATE TABLE IF NOT EXISTS messages (id INT CHECK (id > 0) NOT NULL DEFAULT NEXTVAL ('messages_seq'), to INT CHECK (to > 0) NOT NULL, from INT CHECK (from > 0) NOT NULL, time INT CHECK (time > 0) NOT NULL, type VARCHAR(255), message longtext NULL, PRIMARY KEY (id))",
+	SQLite: 'CREATE TABLE IF NOT EXISTS messages (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `to` INTEGER UNSIGNED NOT NULL, `from` INTEGER UNSIGNED NOT NULL, `time` INTEGER UNSIGNED NOT NULL, `type` VARCHAR(255), `message` longtext NULL)',
+}
 func main(){
 	//TODO: add multible database options
-	db,err:=sql.Open("sqlite3","./ggm.db")
+	db,err:=sql.Open(driver[SQLite],"./ggm.db")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	_,err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
-		id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-		name VARCHAR(255),
-		domain VARCHAR(255),
-		password VARCHAR(255),
-		key VARCHAR(16),
-		port INTEGER UNSIGNED NOT NULL,
-		PRIMARY KEY (id)
-	)`)
+	_,err = db.Exec(CREATEUSERS[SQLite])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	_,err = db.Exec(`CREATE TABLE IF NOT EXISTS messages (
-		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-		to INT UNSIGNED NOT NULL,
-		from INT UNSIGNED NOT NULL,
-		time INT UNSIGNED NOT NULL,
-		type VARCHAR(255),
-		message longtext NULL,
-		PRIMARY KEY (id)
-	)`)
+	_,err = db.Exec(CREATEMESSAGES[SQLite])
 	if err != nil {
 		fmt.Println(err)
 		return
